eed3ce2a4abc3c34d05683d68b59433e83d38ad5
Lucky_He <673230244@qq.com>
2022-05-03 00:18:35 +0800

    VarLong优化

--- src/main/java/fun/boomcat/luckyhe/spigot/plugin/luckyminecraftqqchatspigot/packet/datatype/VarLong.java
+++ src/main/java/fun/boomcat/luckyhe/spigot/plugin/luckyminecraftqqchatspigot/packet/datatype/VarLong.java
@@ -9,15 +9,22 @@ import java.util.Arrays;
 
 public class VarLong {
     private final long value;
+    private byte[] bytes;
+    private int len = -1;
 
     @Override
     public String toString() {
         return "VarLong{" +
                 "value=" + value +
+                ", bytes=" + Arrays.toString(bytes) +
+                ", len=" + len +
                 '}';
     }
 
     public byte[] getBytes() {
+        if (this.bytes != null) {
+            return this.bytes;
+        }
         long value = this.value;
         byte[] bytes = new byte[10];
         int i = 0;
@@ -25,17 +32,23 @@ public class VarLong {
         while (true) {
             if ((value & ~0x7F) == 0) {
                 bytes[i] = ((byte) value);
-                return Arrays.copyOfRange(bytes, 0, i + 1);
+                this.bytes =  Arrays.copyOfRange(bytes, 0, i + 1);
+                break;
             }
 
             bytes[i] = ((byte) ((value & 0x7F) | 0x80));
             value >>>= 7;
             i++;
         }
+
+        return this.bytes;
     }
 
     public int getBytesLength() {
-        return getBytes().length;
+        if (len == -1) {
+            len = getBytes().length;
+        }
+        return len;
     }
 
     public long getValue() {
