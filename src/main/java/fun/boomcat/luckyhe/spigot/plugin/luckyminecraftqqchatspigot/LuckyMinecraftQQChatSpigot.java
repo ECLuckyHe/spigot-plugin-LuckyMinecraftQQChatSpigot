4601bf28b9d49b6e16518b59d54a7cc42c515c28
Lucky_He <673230244@qq.com>
2022-07-04 10:43:07 +0800

    换设备测试

--- src/main/java/fun/boomcat/luckyhe/spigot/plugin/luckyminecraftqqchatspigot/LuckyMinecraftQQChatSpigot.java
+++ src/main/java/fun/boomcat/luckyhe/spigot/plugin/luckyminecraftqqchatspigot/LuckyMinecraftQQChatSpigot.java
@@ -30,6 +30,8 @@ public class LuckyMinecraftQQChatSpigot extends JavaPlugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // huanshebeiceshi
     }
 
     @Override
